4b8eefbb68f4c3c6cc9437e4ecca4096f62c43e3
xiaobo <peterwillcn@gmail.com>
2024-07-04 18:19:43 +0800

    feat: add the automated deployment draft v0.0.1
    Signed-off-by: xiaobo <peterwillcn@gmail.com>

---
+++ docker/auto-deploy/go.mod
@@ -0,0 +1,7 @@
+module github.com/PancrePal-xiaoyibao/PancrePal-xiaoyibao
+
+go 1.22
+
+require (
+	github.com/peterwillcn/ai-lauch v0.0.2
+)
---
+++ docker/auto-deploy/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/peterwillcn/ai-lauch/libs"
+)
+
+func main() {
+	container := libs.NewContainer()
+
+	// 0
+	templates, err := libs.ReadTemplate(container.TmplDir)
+	if err != nil {
+		fmt.Errorf("failed to parse templates: %w", err)
+	}
+
+	// 1
+	err = os.MkdirAll(container.WorkDir, 0755)
+	if err != nil {
+		fmt.Printf("Error creating directory %s: %s\n", container.WorkDir, err)
+	}
+
+	// 2
+	for _, mainFest := range container.MainFests {
+		libs.ApplyTemplate(container.WorkDir, templates, mainFest, container)
+	}
+
+	// 3
+	libs.Start(container.WorkDir, container.MainFests)
+
+}
